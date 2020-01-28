5e5c8052b5806848137db876fd8ade45ea5fdb0a
pchen128 <pchen128@my.bcit.ca>
2020-01-27 19:34:06 -0800

    Updates

---
+++ precache-manifest.4a49010a85f88f95e615f67429fdeb33.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52ac792f81eae9548eca5125cf368081",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6283830b08721b761b0",
+    "url": "/static/css/main.e7841ac9.chunk.css"
+  },
+  {
+    "revision": "c460e32a0196bce44323",
+    "url": "/static/js/2.3a80b4b0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.3a80b4b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e6283830b08721b761b0",
+    "url": "/static/js/main.d86530f5.chunk.js"
+  },
+  {
+    "revision": "a02e0fa040d780c6da0f",
+    "url": "/static/js/runtime-main.b56ca929.js"
+  },
+  {
+    "revision": "5ea7eb0f21de30009004ef68d1388782",
+    "url": "/static/media/billionaire.5ea7eb0f.png"
+  },
+  {
+    "revision": "9fea5c02f58fc50fb41521d21dd89244",
+    "url": "/static/media/billionaireIOS.9fea5c02.gif"
+  },
+  {
+    "revision": "28f49ae2331616858926eea6ee3ed897",
+    "url": "/static/media/billionaireios.28f49ae2.png"
+  },
+  {
+    "revision": "56ec4ee72a91e8f3d954db6cccbf8ab2",
+    "url": "/static/media/cabin.56ec4ee7.png"
+  },
+  {
+    "revision": "a29c0b43958bf3638364e15ff646fffa",
+    "url": "/static/media/cake.a29c0b43.png"
+  },
+  {
+    "revision": "e27c88e555910ddce50b6f1e275023a4",
+    "url": "/static/media/circus.e27c88e5.png"
+  },
+  {
+    "revision": "107d31e81b23d07ad38c7cc4d1f7ea27",
+    "url": "/static/media/game.107d31e8.png"
+  },
+  {
+    "revision": "42e99408d26fd436c05fc74da86df822",
+    "url": "/static/media/head.42e99408.png"
+  },
+  {
+    "revision": "7dfdcf559d331a0cdc05cc1d2b8c8233",
+    "url": "/static/media/safe.7dfdcf55.png"
+  },
+  {
+    "revision": "c64175e5cf01d5ef498c79557f2b86a3",
+    "url": "/static/media/santalist.c64175e5.png"
+  },
+  {
+    "revision": "8fad4ff92040b17345896e0e777a79ad",
+    "url": "/static/media/submarine.8fad4ff9.png"
+  }
+]);
